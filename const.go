17410d2d7e5562585a74a1cc4dda73d1c9dd1f97
李光春 <gc@dtapp.net>
2022-09-21 01:42:32 +0800

    - update

--- const.go
+++ const.go
@@ -1,7 +1,10 @@
 package ejiaofei
 
 const (
-	apiUrl   = "http://api.ejiaofei.net:11140"
-	logTable = "ejiaofei"
-	Version  = "1.0.7"
+	apiUrl = "http://api.ejiaofei.net:11140"
+)
+
+const (
+	LogTable = "ejiaofei"
+	Version  = "1.0.8"
 )
